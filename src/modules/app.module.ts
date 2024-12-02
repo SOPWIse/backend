2784212dcbe20d6ed914b70277c3fab71f1bfc51
Arun Mishra <136677308+arun-mishra20@users.noreply.github.com>
2024-12-02 23:24:29 +0530

    s3 updates

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -6,13 +6,14 @@ import { PaginationService } from '@sopwise/common/pagination/pagination.service
 import { LoggerMiddleware } from '@sopwise/middlewares/logger.middleware';
 import { AuthModule } from '@sopwise/modules/auth/auth.module';
 import { AuthService } from '@sopwise/modules/auth/auth.service';
+import { FileManagerModule } from '@sopwise/modules/file-manager/file-manager.module';
 import { HealthModule } from '@sopwise/modules/health/health.module';
 import { UserModule } from '@sopwise/modules/user/user.module';
 import { UserService } from '@sopwise/modules/user/user.service';
 import { PrismaModule } from '@sopwise/prisma/prisma.module';
 import { PrismaService } from '@sopwise/prisma/prisma.service';
-import { ApprovalsService } from './approvals/approvals.service';
 import { ApprovalsModule } from './approvals/approvals.module';
+import { ApprovalsService } from './approvals/approvals.service';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { ApprovalsModule } from './approvals/approvals.module';
     PrismaModule,
     ConfigModule.forRoot(),
     HealthModule,
+    FileManagerModule,
     // Rate limiting for all routes
     ThrottlerModule.forRoot([
       {
---
+++ src/modules/file-manager/aws/s3/s3.service.ts
@@ -0,0 +1,90 @@
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from '@aws-sdk/client-s3';
+import { Injectable } from '@nestjs/common';
+import { S3ClientFactory } from '@sopwise/modules/file-manager/aws/s3/s3.factory';
+import { Readable } from 'node:stream';
+
+interface S3ObjectAccessArgs {
+  objectKey: string;
+  bucket: string;
+}
+
+interface S3UploadArgs extends S3ObjectAccessArgs {
+  file: Buffer;
+  mimeType?: string;
+  isPublic?: boolean;
+}
+
+@Injectable()
+export class S3Service {
+  constructor(private readonly s3Factory: S3ClientFactory) {}
+
+  async upload({
+    file,
+    objectKey,
+    mimeType,
+    bucket,
+    isPublic = true,
+  }: S3UploadArgs): Promise<string> {
+    try {
+      const commandInput: PutObjectCommandInput = {
+        Key: objectKey,
+        Bucket: bucket,
+        Body: file,
+        ContentType: mimeType,
+        ContentDisposition: 'inline',
+      };
+
+      // if (isPublic) commandInput.ACL = 'public-read'
+      const command = new PutObjectCommand(commandInput);
+
+      await this.s3Factory.client.send(command);
+      const encodeFileName = encodeURIComponent(objectKey);
+      return `https://${bucket}.s3.amazonaws.com/${encodeFileName}`;
+    } catch (error) {
+      throw new Error('An error occurred uploading file to storage');
+    }
+  }
+
+  async replace({
+    file,
+    objectKey,
+    mimeType,
+    bucket,
+    isPublic = true,
+  }: S3UploadArgs): Promise<void> {
+    await this.deleteObject({ objectKey, bucket });
+    await this.upload({ file, objectKey, mimeType, bucket, isPublic });
+  }
+
+  async deleteObject({ objectKey, bucket }: S3ObjectAccessArgs) {
+    try {
+      const command = new DeleteObjectCommand({
+        Bucket: bucket,
+        Key: objectKey,
+      });
+      await this.s3Factory.client.send(command);
+    } catch (error) {
+      throw new Error('An error occurred deleting the file from storage');
+    }
+  }
+
+  async getObject({ objectKey, bucket }: S3ObjectAccessArgs) {
+    const command = new GetObjectCommand({ Bucket: bucket, Key: objectKey });
+    try {
+      const object = await this.s3Factory.client.send(command);
+      return {
+        contentType: object.ContentType,
+        contentStream: object.Body as Readable,
+      };
+    } catch (error) {
+      if (error.name === 'NoSuchKey')
+        throw new Error('The file does not exist in storage');
+      throw new Error('An error occurred downloading the file from storage');
+    }
+  }
+}
