af6440c1eca935e4216047b34084b0389d098cb6
Arun Mishra <136677308+arun-mishra20@users.noreply.github.com>
2024-12-02 23:29:01 +0530

    s3 updates

--- src/modules/file-manager/aws/s3/s3.factory.ts
+++ src/modules/file-manager/aws/s3/s3.factory.ts
@@ -1,29 +1,31 @@
-import { S3, S3ClientConfig } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { S3, S3ClientConfig } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
-export type IGetSignedUrl = { getSignedUrl: typeof getSignedUrl }
+export type IGetSignedUrl = { getSignedUrl: typeof getSignedUrl };
 @Injectable()
 export class S3ClientFactory {
-	constructor(private configService: ConfigService) {} // Add ConfigService as a constructor parameter
-	get client(): S3 {
-		// const accessKeyId = 'AKIAY5KMC4GMU7F5D5GH'
-		const accessKeyId = this.configService.get<string>('aws.accessKeyId')
-		const secretAccessKey = this.configService?.get<string>('aws.secretAccessKey')
-		const region = this.configService?.get<string>('aws.region')
+  constructor(private configService: ConfigService) {}
+  get client(): S3 {
+    // const accessKeyId = 'AKIAY5KMC4GMU7F5D5GH'
+    const accessKeyId = this.configService.get<string>('aws.accessKeyId');
+    const secretAccessKey = this.configService?.get<string>(
+      'aws.secretAccessKey',
+    );
+    const region = this.configService?.get<string>('aws.region');
 
-		const options: S3ClientConfig = {
-			region: region,
-			credentials: {
-				accessKeyId,
-				secretAccessKey,
-			},
-		}
-		return new S3(options)
-	}
+    const options: S3ClientConfig = {
+      region: region,
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+    };
+    return new S3(options);
+  }
 
-	get urls(): IGetSignedUrl {
-		return { getSignedUrl }
-	}
+  get urls(): IGetSignedUrl {
+    return { getSignedUrl };
+  }
 }
